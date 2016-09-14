bc21e782644aac104343a729462b983893b41cce
Neil Atkinson <neil@bluespot.io>
2016-09-14 15:18:49 +0100

    Rebuild for android.

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -33,6 +33,7 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-
+        var url = 'https://www.google.co.uk'
+        cordova.InAppBrowser.open(url, '_self', 'location=no');
     },
 };
